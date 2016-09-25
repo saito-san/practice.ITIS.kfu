e90d0bc8e62c235f280b3beeb8afcef76dc71f1f
saito-san <alexsparco95@gmail.com>
2016-09-25 17:37:07 +0300

    Домашняя работа №1 часть 1 №16

---
+++ lucky_ticket/lucky_ticket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lucky_ticket
+{
+    class lucky_ticket
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите цифры с билета последвательно по одной");
+            int a, b, c, d, e, f;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            f = int.Parse(Console.ReadLine());
+                        
+            if ((a + b + c == d + e + f + 1) || (a + b + c == d + e + f - 1))
+                Console.WriteLine("Ваш билет почти счастливый!");
+            else
+                Console.WriteLine("Ваш билет не почти счастливый");
+            Console.ReadKey();
+        }
+    }
+}
