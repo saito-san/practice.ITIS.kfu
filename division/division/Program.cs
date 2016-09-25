509d4f08f826a0fae1413aef098b7f9dcb15fbf1
saito-san <alexsparco95@gmail.com>
2016-09-25 19:20:48 +0300

    Домашняя работа №1 часть 2 №11

---
+++ division/division/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace division
+{
+    class division
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число в 2СС");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введите делитель в 10СС");
+            double b = double.Parse(Console.ReadLine());
+
+            double c = 0; 
+            int k = a.Length;
+            for (int i = 0 ; k > 0; i++)
+            {
+                if (a[i] == '1')
+                    c = c + Math.Pow(2, k-1);
+                k--;
+            }
+            if (c % b == 0)
+                Console.WriteLine("{0} Делится нацело  ", c);
+            else
+                Console.WriteLine("{0} не делится нацело ", c);
+            Console.ReadKey();
+        }
+    }
+}
