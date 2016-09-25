83e58a9b60df56bd99dfe11d28169e70c25805dd
saito-san <alexsparco95@gmail.com>
2016-09-25 18:31:13 +0300

    Домашняя работа №1 часть 2 №9

---
+++ counter/counter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace counter
+{
+    class counter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите двоичное представление числа");
+            string a = Console.ReadLine();
+            int k = 0;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == '1')
+                    k++;
+            Console.WriteLine("количество единиц в двоичной записи числа - {0}", k);
+            Console.ReadKey();
+
+        }
+    }
+}
