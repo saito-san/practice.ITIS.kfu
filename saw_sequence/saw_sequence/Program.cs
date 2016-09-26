4181ed16368d6699c3e5b61680a56a09e99ce57f
saito-san <alexsparco95@gmail.com>
2016-09-26 22:05:27 +0300

    Домашняя работа №1 часть 3 №8

---
+++ saw_sequence/saw_sequence/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saw_sequence
+{
+    class saw_sequence
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("вводите элементы последовательности");
+            double b, a = double.Parse(Console.ReadLine());
+            char c = '-';
+            int i = 0, j = 0, k = 0;
+            b = a;
+            do
+            {
+                a = b;
+                b = double.Parse(Console.ReadLine());
+                if (a > b)
+                    i++;
+                else
+                    j++;
+
+                Console.WriteLine("Хотите вводить дальше? + : да; - : нет");
+                c = char.Parse(Console.ReadLine());
+                if (c == '-')
+                {
+                    Console.WriteLine("последовательность пилообразная!");
+                    Console.ReadKey();
+                    break;
+                }
+                k++;
+            }
+            while (((i - j + k) <= 2) && ((j - i + k) <= 2));
+            if (((i - j + k) <= 2) && ((j - i + k) <= 2))
+            {
+                Console.WriteLine("последовательность не пилообразная!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
