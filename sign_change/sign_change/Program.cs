f14856db2e35f3a47523e64706aa0d912e38bb3a
saito-san <alexsparco95@gmail.com>
2016-09-26 22:15:47 +0300

    Домашняя работа №1 часть 3 №10

---
+++ sign_change/sign_change/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sign_change
+{
+    class sign_change
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("вводите элементы последовательности");
+            double b, a = double.Parse(Console.ReadLine());
+            char c = '-';
+            int i = 0;
+            b = a;
+            do
+            {
+                a = b;
+                b = double.Parse(Console.ReadLine());
+                if (((a > 0) && (b < 0)) || ((a < 0) && (b > 0)))
+                    i++;
+                
+                Console.WriteLine("Хотите вводить дальше? + : да; - : нет");
+                c = char.Parse(Console.ReadLine());
+                if (c == '-')
+                {
+                    Console.WriteLine("смена знака произошла {0} раз", i);
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            while (true);
+        }
+    }
+}
