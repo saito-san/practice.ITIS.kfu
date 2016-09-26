10cb99c4df557eb3b02eeb1ed08088ababa641e6
saito-san <alexsparco95@gmail.com>
2016-09-26 13:08:47 +0300

    Домашняя работа №1 часть 3 №6

---
+++ increasing_sequence/increasing_sequence/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace increasing_sequence
+{
+    class increasing_sequence
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("вводите элементы последовательности");
+            double b, a = double.Parse(Console.ReadLine());
+            char c = '-';
+            b = a;
+            do
+            {
+                a = b;
+                b = double.Parse(Console.ReadLine());
+                if (a > b)
+                {
+                    Console.WriteLine("последовательность не возрастающая!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Хотите вводить дальше? + : да; - : нет");
+                    c = char.Parse(Console.ReadLine());
+                    if (c == '-')
+                    {
+                        Console.WriteLine("последовательность возрастающая!");
+                        break;
+                    }
+                }
+            }
+            while (a < b);
+            Console.ReadKey();
+        }
+    }
+}
