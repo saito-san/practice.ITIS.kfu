ad7e3d94c9975d689e85e6b511516633a7f42ddf
saito-san <alexsparco95@gmail.com>
2016-10-11 00:38:10 +0300

    Problem Set #2 task 1

---
+++ e_grade_x_approximate/e_grade_x_approximate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace e_grade_x_approximate
+{
+    class Program //работает для (n <= 28)&&(x <= 1), иначе - идет переполнение из-за факториала
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите точность - кол-во знаков после запятой");
+            int n = int.Parse(ReadLine());
+            WriteLine("Введите x");
+            decimal e = 1, fact = 1, x = decimal.Parse(ReadLine());
+            
+            string result;
+            int i = 1;
+            do
+            {
+                e += x/fact;
+                result = Convert.ToString(Math.Round(e, i - 1));
+                i++;
+                x *= x;
+                fact *= i;
+                WriteLine("{0}",result);
+            }
+            while ((result.Length - result.IndexOf('.') - 1) < n);
+            WriteLine("{0:} /n {1} /n {2}", result, i, Math.Exp(Convert.ToDouble(x)));
+            ReadKey();
+            
+        }
+    }
+}
