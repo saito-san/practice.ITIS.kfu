24c19734d2ccb10c8a5eaa278f023fe33c16aa36
saito-san <alexsparco95@gmail.com>
2016-10-16 20:54:57 +0300

    Problem set #2 task iteration-1

---
+++ third-root_approximate/third-root_approximate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace third_root_approximate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите точность");
+            decimal thirdroot0 = 0, thirdroot1,
+                    n = decimal.Parse(ReadLine());
+            int i = 0;
+            WriteLine("Введите х");
+            decimal x = decimal.Parse(ReadLine());
+
+            if (x < 1)
+                thirdroot1 = x;
+            else
+                thirdroot1 = x / 3;
+            
+            do
+            {
+                thirdroot0 = thirdroot1;
+                thirdroot1 = thirdroot0 - (thirdroot0 - (x / thirdroot0 / thirdroot0)) / 3;
+                i++;
+            }
+            while ((thirdroot0 - thirdroot1) > n);
+            WriteLine($"\nвычисление верно в широком смысле до этого разряда\n{n}\n{thirdroot1}\nза {i} шагов\n");
+            ReadKey();
+        }
+    }
+}
