8f85393317db54105825083a1a343bcd4ebf767e
LENOVO-S12\Александр <alexsparco95@gmail.com>
2016-12-12 13:09:18 +0300

    Летучка 12.12.2016

---
+++ Летучка 12.12.2016/Летучка 12.12.2016/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Летучка_12._12._2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Publication> a = new List<Publication>();
+            for (int i = 0; i < 10; i++)
+            {
+                DateTime b = new DateTime(2016, 12, 12);
+                DateTime c = new DateTime(2016, 12, 22);
+
+                a.Add(new Ad($"Пропала собака{i}", "глаза черные, уши длинные, хвост пистолетом", b, c));
+                a.Add(new Report($"статья об окружающей среде{i}", "окружающая среда очень важна для нас", "Иванов И. И."));
+                a.Add(new News($"Трамп победил", "Дональд Трамп победил на выборах в США", b = new DateTime(2016, 11, 20), "New York Times"));
+            }
+
+            for (int i = 0; i < a.Count - 1; i++)
+            {
+                if (a[i] is Ad)
+                {
+                    Ad b = (Ad)a[i];
+                    if (b.delete_date > DateTime.Now)
+                        Console.WriteLine(a[i]);
+                }
+                else
+                    Console.WriteLine(a[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Летучка 12.12.2016/Летучка 12.12.2016/publication.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Летучка_12._12._2016
+{
+    public class Publication
+    {
+        public string header { get; set; }
+        public string text { get; set;  }
+
+        public Publication(string header, string text)
+        {
+            this.header = header;
+            this.text = text;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{header}\n{text}");
+        }
+    }
+
+    public class News : Publication
+    {
+        public DateTime date { get; set; }
+        public string istochnik { get; set; }
+
+        public News( string header, string text, DateTime date, string istochnik) : base(header, text)
+        {
+            this.date = date;
+            this.istochnik = istochnik;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine(base.ToString());
+            return String.Format($"дата публикации: {date}\nисточник: {istochnik}\n\n");
+        }
+    }
+
+    public class Ad : Publication
+    {
+        public DateTime post_date { get; set; }
+        public DateTime delete_date { get; set; }
+
+        public Ad(string header, string text, DateTime post_date, DateTime delete_date) : base(header, text)
+        {
+            this.post_date = post_date;
+            this.delete_date = delete_date;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine(base.ToString());
+            return String.Format($"дата публикации: {post_date}\nдата окончания: {delete_date}\n\n");
+
+        }
+    }
+
+    public class Report : Publication
+    {
+        public string author { get; set; }
+
+        public Report(string header, string text, string author) : base(header, text)
+        {
+            this.author = author;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine(base.ToString());
+            return String.Format($"автор: {author}\n\n");
+        }
+    }
+}
